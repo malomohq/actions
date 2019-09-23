b308ed9cc55401930a1b685e445e464bd6a4ab35
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-09-22 20:07:59 -0400

    [node/package-version] fix usage of path.join

--- node/package-version/index.js
+++ node/package-version/index.js
@@ -3,9 +3,9 @@ const core = require('@actions/core');
 const path = require('path');
 
 try {
-  const packagePath = path.join([core.getInput('path'), 'package.json']);
+  const packagePath = path.join(core.getInput('path'), 'package.json');
 
-  const package = require(packagePath);
+  const package = require(`./${packagePath}`);
 
   core.setOutput('version', package.version);
 } catch (error) {
