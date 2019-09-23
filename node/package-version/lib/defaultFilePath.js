a678db20eb633a399d9a1649aca85a97dd6063a5
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-09-23 17:18:35 -0400

    Attempt to fix usage of defaultFilePath.js

--- node/package-version/lib/defaultFilePath.js
+++ node/package-version/lib/defaultFilePath.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 module.exports = () => {
   const context = github.context;
 
+  console.log(context.repo);
+
   const repoName = context.repo.split('/');
 
   return `/home/runner/work/${repoName}/${repoName}/package.json`;
