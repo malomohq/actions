711dcc0b1b726ff881b25e1983645a6a29353379
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-09-23 11:56:03 -0400

    [node/package-version] input is now path to package.json; not path to directory containing package.json

--- node/package-version/index.js
+++ node/package-version/index.js
@@ -1,11 +1,7 @@
 const core = require('@actions/core');
 
-const path = require('path');
-
 try {
-  const packagePath = path.join(core.getInput('path'), 'package.json');
-
-  const package = require(`./${packagePath}`);
+  const package = require(core.getInput('filePath'));
 
   core.setOutput('version', package.version);
 } catch (error) {
