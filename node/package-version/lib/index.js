de6fcd542e2a2806eb9ecac983810b1a9798b9e6
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-09-23 17:14:54 -0400

    Attempt to fix usage of defaultFilePath.js

--- node/package-version/lib/index.js
+++ node/package-version/lib/index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 const defaultFilePath = require('./defaultFilePath');
 
 try {
-  const filePath = core.getInput('filePath') || defaultFilePath
+  const filePath = core.getInput('filePath') || defaultFilePath();
   const prefix = core.getInput('prefix') || '';
   const suffix = core.getInput('suffix') || '';
 
